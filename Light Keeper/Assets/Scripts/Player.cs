20fcafdb6d5ee0ae7c4b20ea9987207ca3395668
gaston <gastonveliez95@gmail.com>
2018-08-30 13:19:44 -0300

    Rotacion de jugador

--- Light Keeper/Assets/Scripts/Player.cs
+++ Light Keeper/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour {
 
     private Rigidbody2D rigi;
 
+    private Vector2 moveDir;
+
     [SerializeField]
     private float movSpeed;
 
@@ -19,30 +21,86 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private GameObject sprite;
 
+    [SerializeField]
+    private float rotSpeed;
+
 	void Start () {
         rigi = GetComponent<Rigidbody2D>();
 	}
 	
 	void FixedUpdate () {
+        Vector2 newPos = rigi.position + moveDir * Time.fixedDeltaTime;
+        rigi.MovePosition(newPos);
 
-        float horMov = Input.GetAxis("Horizontal");
-        float verMov = Input.GetAxis("Vertical");
-
-        Vector2 pos = new Vector2(horMov * movSpeed, verMov * movSpeed);
-
-
-        pos *= Time.deltaTime;
-
-        HandleRotation(horMov,verMov);
-        transform.Translate(pos);
 
 	}
 
-    void HandleRotation(float x,float y)
+    private void Update()
+    {
+        float movHor = Input.GetAxisRaw("Horizontal");
+        float movVer = Input.GetAxisRaw("Vertical");
+
+        //Smooth version
+        //Vector2 mov = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+
+        //Robotic version
+        Vector2 mov = new Vector2(movHor,movVer);
+
+        moveDir = mov * movSpeed;
+        HandleRotation(movHor, movVer);
+
+
+    }
+
+    void HandleRotation(float x, float y)
     {
 
-        
 
+        if(x>0)
+        {
+            if(y>0)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, -45), rotSpeed * Time.deltaTime);
+            }
+            else if (y<0)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, -125), rotSpeed * Time.deltaTime);
+            }
+            else
+            {
+               
+                transform.rotation = Quaternion.Slerp(transform.rotation ,Quaternion.Euler(0, 0, -90),rotSpeed* Time.deltaTime);
+            }
+        }
+        else if(x<0)
+        {
+            if (y>0)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 45), rotSpeed * Time.deltaTime);
+            }
+            else if (y<0)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 125), rotSpeed * Time.deltaTime);
+            }
+            else
+            {
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 90), rotSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (y>0)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), rotSpeed * Time.deltaTime);
+            }
+            else if (y<0)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 180), rotSpeed * Time.deltaTime);
+            }
+
+        }
+        
     }
 
     void Shoot()
